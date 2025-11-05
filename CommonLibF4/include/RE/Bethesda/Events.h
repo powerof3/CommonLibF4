0c96f4dc5002915e472039a973f20598191333a1
powerof3 <32599957+powerof3@users.noreply.github.com>
2025-11-05 00:38:57 +0000

    maintenance

--- CommonLibF4/include/RE/Bethesda/Events.h
+++ CommonLibF4/include/RE/Bethesda/Events.h
@@ -537,7 +537,7 @@ namespace RE
 	public:
 		bool operator==(const HUDSubtitleDisplayData& a_rhs) { return speakerName == a_rhs.speakerName && subtitleText == a_rhs.subtitleText; }
 		bool operator!=(const HUDSubtitleDisplayData& a_rhs) { return !operator==(a_rhs); }
-		
+
 		// members
 		BSFixedStringCS speakerName;   // 00
 		BSFixedStringCS subtitleText;  // 08
